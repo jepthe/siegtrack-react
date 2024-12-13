bc767474f23d779542b5c53c9b018e70d0e67013
jepthe <jepthe.hf@gmail.com>
2024-12-13 14:18:03 -0600

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
 //azure
 // src/config/config.js
+console.log('Todas las variables:', import.meta.env); // Ver todas las variables
+console.log('VITE_API_URL específica:', import.meta.env.VITE_API_URL); // Ver la variable específica
+
 const config = {
   apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:5002'
 };
 
-// Para debug
-console.log('Variable de entorno:', import.meta.env.VITE_API_URL);
-console.log('Config apiUrl:', config.apiUrl);
+console.log('URL final:', config.apiUrl); // Ver qué URL se está usando
 
 export default config;
