593fa6361bc0f279fc3d309f2fe20a8968399bd7
jepthe <jepthe.hf@gmail.com>
2024-12-13 01:17:58 -0600

    create web.config and others

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  hmr: true,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0'
+  }
 })
